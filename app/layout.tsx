83ce82f789c70c136fea7058c586f853df6fc7fa
Simon Ermolinsky <gfromsx@gmail.com>
2024-04-04 19:38:20 +0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  сhildren,
 }: Readonly<{
-  children: React.ReactNode;
+  сhildren: React.ReactNode;
 }>) {
   return (
     <html lang="en">
